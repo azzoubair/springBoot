1ce926f383a4629be58e8c8c53d74998f30e2350
azzoubair <43780120+azzoubair@users.noreply.github.com>
2019-03-18 14:57:13 +0100

    Add files via upload

---
+++ src/main/java/com/cgi/fic/api/OpenshifTestApplication.java
@@ -0,0 +1,14 @@
+package com.cgi.fic.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenshifTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenshifTestApplication.class, args);
+		 System.out.println("Hello, World");
+	}
+
+}
